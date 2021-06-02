910ff6062e6e569025ba4b4addde9fac46356d40
Fabrice Maillefer <fabrice.maillefer@gmail.com>
2021-06-02 20:21:02 +0200

    Add audio

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
     $(document).ready(function () {
 
         // http://www.jsfuck.com/
-        var pin = 123456;
+        var pin = 1;
         var enterCode = "";
         enterCode.toString();
 
@@ -14,7 +14,7 @@ jQuery(document).ready(function ($) {
             lengthCode--;
             $("#fields .numberfield:eq(" + lengthCode + ")").addClass("active");
 
-            if (lengthCode == 5) {
+            if (lengthCode == 0) {
 
                 // Check the PIN
                 if (enterCode == pin) {
@@ -40,10 +40,13 @@ jQuery(document).ready(function ($) {
         });
 
         var listElem = [
-            { name: 'Tibeau Lambveret', audio: 'morse1.mp3' },
-            { name: 'Tibeau2 Lambveret', audio: 'morse2.mp3' },
-            { name: 'Tibeau3 Lambveret', audio: 'morse3.mp3' },
-            { name: 'Tibeau4 Lambveret', audio: 'morse4.mp3' },
+            { name: 'Tibeau Lambveret', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
+            { name: 'John Doe', audio: 'morse-BONJOUR.mp3' },
+            { name: 'Alain Berset', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
+            { name: 'Jaques Rocher', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
+            { name: 'Anne Jaquet', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
+            { name: 'Clélia Jouve', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
+            { name: 'Obi Wan', audio: 'morse-RAYON_DE_SOLEIL.mp3' },
         ]
         $.each(listElem, function (index, value) {
             $("#list-messages-audios").append(
@@ -54,7 +57,7 @@ jQuery(document).ready(function ($) {
                 '    </div>' +
                 '    <div class="item-sound">' +
                 '        <audio controls>' +
-                '            <source src="' + value.audio + '" type="audio/mpeg">' +
+                '            <source src="audio/' + value.audio + '" type="audio/mpeg">' +
                 '        </audio>' +
                 '    </div>' +
                 '</div>'
